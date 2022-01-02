62a616ad4690d73599b6a73918ad1e8e7785b9ec
Piotr Chowaniec <piter.chowaniec@gmail.com>
2022-01-02 19:29:52 +0100

    exclude password from user result thats passed with response

---
+++ server/src/api/users/helpers.ts
@@ -0,0 +1,6 @@
+import { User } from '@common-packages/data-access-layer';
+
+export const mapUserToResponse = (user: User) => ({
+  ...user,
+  password: 'skipped',
+});
--- server/src/api/users/users.service.ts
+++ server/src/api/users/users.service.ts
@@ -3,19 +3,24 @@ import { User } from '@common-packages/data-access-layer';
 
 import { PrismaService } from '../common/prisma/prisma.service';
 
+import { mapUserToResponse } from './helpers';
+
 @Injectable()
 export class UsersService {
   constructor(private prismaService: PrismaService) {}
 
   async findAll(): Promise<User[]> {
-    return this.prismaService.user.findMany();
+    const users = await this.prismaService.user.findMany();
+    return users.map(mapUserToResponse);
   }
 
   async findOne(email): Promise<User | undefined> {
-    return this.prismaService.user.findUnique({
+    const user = await this.prismaService.user.findUnique({
       where: {
         email,
       },
     });
+
+    return mapUserToResponse(user);
   }
 }
