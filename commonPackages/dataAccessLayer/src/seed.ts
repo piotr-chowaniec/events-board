6090616858983b61889f48614db46564359b98a1
Piotr Chowaniec <piter.chowaniec@gmail.com>
2022-02-06 19:31:35 +0100

    Restore previous seed file

--- commonPackages/dataAccessLayer/src/seed.ts
+++ commonPackages/dataAccessLayer/src/seed.ts
@@ -2,75 +2,35 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  // await prisma.user.upsert({
-  //   where: { email: 'admin@events.com' },
-  //   update: {},
-  //   create: {
-  //     email: 'admin@events.com',
-  //     role: 'ADMIN',
-  //     firstName: 'Admin',
-  //     lastName: 'Admin',
-  //     password: 'password',
-  //     events: {
-  //       create: {
-  //         title: 'First Event',
-  //         description: 'Event Description',
-  //         eventDate: new Date(),
-  //       },
-  //     },
-  //   },
-  // });
+  await prisma.user.upsert({
+    where: { email: 'admin@events.com' },
+    update: {},
+    create: {
+      email: 'admin@events.com',
+      role: 'ADMIN',
+      firstName: 'Admin',
+      lastName: 'Admin',
+      password: 'password',
+      events: {
+        create: {
+          title: 'First Event',
+          description: 'Event Description',
+          eventDate: new Date(),
+        },
+      },
+    },
+  });
 
-  // await prisma.user.upsert({
-  //   where: { email: 'piotr@events.com' },
-  //   update: {},
-  //   create: {
-  //     email: 'piotr@events.com',
-  //     role: 'USER',
-  //     firstName: 'Piotr',
-  //     lastName: 'Chowaniec',
-  //     password: '123',
-  //   },
-  // });
-
-  await prisma.event.createMany({
-    data: [
-      {
-        userId: 'ckyg4fen00000c4mvbvywcohq',
-        title: 'Event - 1',
-        eventDate: new Date(),
-        shortDescription: 'Event short description',
-        description: 'Event description',
-      },
-      {
-        userId: 'ckyg4fen00000c4mvbvywcohq',
-        title: 'Event - 2',
-        eventDate: new Date(),
-        shortDescription: 'Event short description',
-        description: 'Event description',
-      },
-      {
-        userId: 'ckyg4fen00000c4mvbvywcohq',
-        title: 'Event - 3',
-        eventDate: new Date(),
-        shortDescription: 'Event short description',
-        description: 'Event description',
-      },
-      {
-        userId: 'ckyg4fen00000c4mvbvywcohq',
-        title: 'Event - 4',
-        eventDate: new Date(),
-        shortDescription: 'Event short description',
-        description: 'Event description',
-      },
-      {
-        userId: 'ckyg4fen00000c4mvbvywcohq',
-        title: 'Event - 5',
-        eventDate: new Date(),
-        shortDescription: 'Event short description',
-        description: 'Event description',
-      },
-    ],
+  await prisma.user.upsert({
+    where: { email: 'piotr@events.com' },
+    update: {},
+    create: {
+      email: 'piotr@events.com',
+      role: 'USER',
+      firstName: 'Piotr',
+      lastName: 'Chowaniec',
+      password: '123',
+    },
   });
 }
 
